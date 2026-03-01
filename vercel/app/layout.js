aecf955e21948f0e0ea3931047fa8c69f20386f9
rajibrt <rajibrt@gmail.com>
2026-03-01 14:26:38 +0600

    feat(analytics): integrate StatCounter tracking to root layout

--- vercel/app/layout.js
+++ vercel/app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Script from "next/script";
 
 export const metadata = {
   title: "WEBTV BD || TV Beyond Borders",
@@ -35,7 +36,43 @@ export default function RootLayout({ children }) {
       <head>
         <script dangerouslySetInnerHTML={{ __html: themeInitScript }} />
       </head>
-      <body>{children}</body>
+      <body>
+        {children}
+        <Script id="statcounter-config" strategy="afterInteractive">
+          {`
+            window.sc_project = 12383019;
+            window.sc_invisible = 1;
+            window.sc_security = "596c1a94";
+          `}
+        </Script>
+        <Script
+          id="statcounter-loader"
+          src="https://www.statcounter.com/counter/counter.js"
+          strategy="afterInteractive"
+        />
+        <noscript>
+          <div className="statcounter">
+            <a title="site stats" href="https://statcounter.com/" target="_blank" rel="noreferrer">
+              <img
+                className="statcounter"
+                src="https://c.statcounter.com/12383019/0/596c1a94/1/"
+                alt="site stats"
+                referrerPolicy="no-referrer-when-downgrade"
+              />
+            </a>
+          </div>
+        </noscript>
+        <a
+          href="https://statcounter.com/p12383019/?guest=1"
+          target="_blank"
+          rel="noreferrer"
+          style={{ display: "none" }}
+          aria-hidden="true"
+          tabIndex={-1}
+        >
+          View My Stats
+        </a>
+      </body>
     </html>
   );
 }
